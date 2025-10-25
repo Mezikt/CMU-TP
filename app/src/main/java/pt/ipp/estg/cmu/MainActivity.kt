2ecb1af43f7f4f96e40226970b0b0301da662795
Mezikt97 <nunomesquita97@gmail.com>
2025-10-25 21:01:56 +0100

    primeiro commit

--- app/src/main/java/pt/ipp/estg/cmu/MainActivity.kt
+++ app/src/main/java/pt/ipp/estg/cmu/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name!, welcome to CMU_TP App",
         modifier = modifier
     )
 }
